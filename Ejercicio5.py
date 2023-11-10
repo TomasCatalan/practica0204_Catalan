22590d9920bb5450b38e8778d8033cf2521576f0
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-10 10:45:29 +0100

    Update Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,9 @@
+x = {}
+
+while True:
+    y = input("ingresa palabras en español y sus traducciones (español:inglés), para terminar, escribe 'terminar'.")
+    
+    if y.lower() == "terminar":
+        break
+    else: 
+        print(y)
