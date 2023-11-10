3051544c49268abb4685eee1786b6ac8fd8b48e7
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-10 10:35:48 +0100

    Update Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,8 +1,16 @@
 x = {}
 
-
-
 y = input("ingrese el nombre")
 x["Nombre"] = y
-for z, a in x.items():
+c = 0
+while c == 0:
     print(x)
+    c =+ 1
+
+b = input("ingrese su edad")
+x["Edad"] = b
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+       
