54b6ce5206ce1204512a037ed7402a4263a95261
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-10 10:38:41 +0100

    Update Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -14,3 +14,23 @@ while c == 0:
     print(x)
     c =+ 1
        
+d = input("ingrese su sexo")
+x["sexo"] = d
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+
+e = input("ingrese su telefono")
+x["telefono"] = e
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
+
+f = input("ingrese su correo electronico")
+x["correo"] = f
+c = 0
+while c == 0:
+    print(x)
+    c =+ 1
